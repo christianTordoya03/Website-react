0d54369948fadf841d7a761fa76b33c9f0fae123
christian <cristian14021997@gmail.com>
2021-03-02 21:29:50 -0500

    Fixed components and add new content

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,32 +1,34 @@
-import React from "react";
-import "../App.css";
-import Button from './Button';
-import "./HeroSection.css";
+ 
+import React from 'react';
+import '../App.css';
+// import  Button from './Button';
+import './HeroSection.css';
 
-const HeroSection = () => {
+function HeroSection() {
   return (
-    <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>TORDOYA CAPITAL</h1>
-      <p>what are you waiting for?</p>
-      <div className="hero-btns">
+      <p>Necesitas vender, comprar o alquilar un inmueble en Lima Norte?</p>
+      {/* <div className='hero-btns'>
         <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
         >
           GET STARTED
         </Button>
         <Button
-          className="btns"
-          buttonStyle="btn--primary"
-          buttonSize="btn--large"
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
         >
-          WATCH TRAILER <i className="far fa-play-circle" />
+          WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
-      </div>
+      </div> */}
     </div>
   );
-};
+}
 
 export default HeroSection;
