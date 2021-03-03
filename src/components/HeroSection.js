94ab0399a1e7d1240fd4d9682b4580df6621a85c
christian <cristian14021997@gmail.com>
2021-03-02 19:54:44 -0500

    Created and fixed components with styles and added mode responsive

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from "react";
+import "../App.css";
+import Button from './Button';
+import "./HeroSection.css";
 
 const HeroSection = () => {
   return (
-    <div className='hero-container'>
+    <div className="hero-container">
       <video src="/videos/video-2.mp4" autoPlay loop muted />
       <h1>TORDOYA CAPITAL</h1>
       <p>what are you waiting for?</p>
       <div className="hero-btns">
-        <Button 
-        className='btns'
-        buttonStyle='btn--outline'
-        buttonSize='btn--large'
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
         >
           GET STARTED
         </Button>
-      </div>     
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH TRAILER <i className="far fa-play-circle" />
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
