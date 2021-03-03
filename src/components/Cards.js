6da8002d226835a64f82fe123775e4cca33d840a
christian <cristian14021997@gmail.com>
2021-03-03 11:29:14 -0500

    Adding links

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,13 +11,13 @@ function Cards() {
           <ul className="cards__items">
             <CardItem
               src="images/card.png"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              text="Tenemos las herramientas y el conocimiento para cubrir su necesidad en el menor tiempo y con mayor margen de utilidad"
               // label="Adventure"
               path="/services"
             />
             <CardItem
               src="images/card-2.png"
-              text="Travel through the Islands of Bali in a Private Cruise"
+              text="Nos encargamos de sanear su inmueble ante registros públicos"
               // label="Luxury"
               path="/services"
             />
@@ -25,19 +25,19 @@ function Cards() {
           <ul className="cards__items">
             <CardItem
               src="images/card-3.png"
-              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+              text="Inscripción de la edificación en registros públicos sin que pague multas"
               // label="Mystery"
               path="/services"
             />
             <CardItem
               src="images/logo.png"
-              text=""
+              text="Profesionales especializados en superar tus expectativas"
               // label="Adventure"
               path="/products"
             />
             <CardItem
               src="images/card-4.png"
-              text="Ride through the Sahara Desert on a guided camel tour"
+              text="Conoce los beneficios de la titulación de un inmueble"
               // label="Adrenaline"
               path="/sign-up"
             />
