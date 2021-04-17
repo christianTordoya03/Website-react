437df17bd3b4a25ebf721fd6314f11417b02e06e
christian <cristian14021997@gmail.com>
2021-04-17 00:06:51 -0500

    Update services

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,24 +1,13 @@
 import React from "react";
 import "../../App.css";
 import "../../components/HeroSection.css";
-import services from "../../images/tcserv.png";
+import services from "../../images/servicios.png";
 
 const Services = () => {
   return (
     <div className="hero-container">
-      <div className="cards">
-        <div className="cards__container">
-          <div className="cards__wrapper">
-            <ul className="cards__items">
-              <CardItem
-                src="images/servicios.png"
-                text="Listo para usar con Angares y oficina inscrito en registros públicos 1477 m2 zona industrial las piedritas Carabayllo información 943450491"
-                path="/services"
-              />
-            </ul>
-          </div>
-        </div>
-      </div>
+      <img src={services} alt="services" className="services--img" />
+      {/* <video src="/videos/video-1.mp4" autoPlay loop muted /> */}
     </div>
   );
 };
